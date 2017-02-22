efecf3994cc03904b1669f3ae01fcdf6925d555f
Geoffrey123 <geoffrey.geoffrey@mail.ru>
2017-02-23 00:45:46 +0400

    python3.x Basit_Islemler
    python3x versiyonunda basit islemler
    Kodu yazdi : Nariman Mammadov

---
+++ Python3.x BasitIslemler.py
@@ -0,0 +1,13 @@
+sayi1 = int(input("Birinci sayiyi girin : "))
+
+sayi2 = int(input("Ikinci sayiyi girin : "))
+
+print("(Toplama) {} + {} = {}".format(sayi1, sayi2, (sayi1+sayi2)))
+
+print("(Cikarma) {} - {} = {}".format(sayi1, sayi2, (sayi1-sayi2)))
+
+print("(Vurma) {} * {} = {}".format(sayi1, sayi2, (sayi1*sayi2)))
+
+print("(Bolme) {} / {} = {}".format(sayi1, sayi2, (sayi1/sayi2)))
+
+print("(Mod alma) {} % {} = {}".format(sayi1, sayi2, (sayi1%sayi2))
