c60c0e09e745fd543466f973c7be557cbd07384d
Geoffrey123 <geoffrey.geoffrey@mail.ru>
2017-02-23 23:15:15 +0400

    Python3.xMantikIslemleri
    kodu yazdi : Nariman Mammadov

---
+++ python3.x Mantikislemleri.py
@@ -0,0 +1,21 @@
+sayi1 = int(input("Birinci sayiyi giriniz :"))
+
+sayi2 = int(input("Ikinci sayiyii giriniz :"))
+
+if sayi1 == sayi2:
+    print("birinci sayi ile ikinci sayi bir-birile esitdir")
+    
+if sayi1 != sayi2:
+    print("birinci sayi ile ikinci sayi bir-birile esit degildir")
+
+if sayi1 < sayi2:
+    print("birinci sayi ikinci sayidan kucukdur")
+
+if sayi1 > sayi2:
+    print("birinci sayi ikinci sayidan buyukdur")
+
+if sayi1 <= sayi2:
+    print("birinci sayi kucuk esitdir")
+
+if sayi1 >= sayi2:
+    print("birinci sayi buyuk esitdir")
