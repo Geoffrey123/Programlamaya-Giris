219989b082611c0ee51daecf1a9c9fc251e011c1
Geoffrey123 <geoffrey.geoffrey@mail.ru>
2017-02-24 00:47:36 +0400

    Python3.x_UcSayidanEnKucugu.py
    Kodu yazdi : Nariman Mammadov

---
+++ Python3.x_UcSayidanEnKucugu.py
@@ -0,0 +1,7 @@
+a = int(input("Birinci Sayiyi Giriniz :"))
+b = int(input("Ikinci Sayiyi Giriniz :"))
+c = int(input("Ucuncu Sayiyi Giriniz :"))
+# max() fonksiyonu en buyuk sayiyi bulur
+# min() fonksiyonu en kicik sayiyi bulur
+print("En buyuk sayi :",max(a,b,c))
+print("En kicik sayi :", min(a,b,c))
